87c025dbf9d1df6a9f3ee6ed58d470540bfd756d
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-10-27 23:53:07 -0700

    agregar servicio y controller de estilo_objeto en unidades.module

--- src/unidades/unidades.module.ts
+++ src/unidades/unidades.module.ts
@@ -9,11 +9,14 @@ import { TemasController } from './controllers/temas.controller';
 import { TemasService } from './services/temas.service';
 import { ObjetosAprendizajeController } from './controllers/objeto-aprendizaje.controller';
 import { ObjetosAprendizajeService } from './services/objetos-aprendizaje.service';
+import { EstiloObjetoController } from './controllers/estilo-objeto.controller';
+import { EstiloObjetoService } from './services/estilo-objeto.service';
+import { EstiloObjeto } from './entities/estilo_objeto.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Unidades, Temas, ObjetosAprendizaje])],
-    controllers: [UnidadesController, TemasController, ObjetosAprendizajeController],
-    providers: [UnidadesService, TemasService, ObjetosAprendizajeService],
+    imports: [TypeOrmModule.forFeature([Unidades, Temas, ObjetosAprendizaje, EstiloObjeto])],
+    controllers: [UnidadesController, TemasController, ObjetosAprendizajeController, EstiloObjetoController],
+    providers: [UnidadesService, TemasService, ObjetosAprendizajeService, EstiloObjetoService],
 
 })
 export class UnidadesModule {
