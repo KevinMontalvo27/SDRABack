a7f169c10c88c627a01b620f383a0cdf9a33419e
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-10-31 00:49:37 -0700

    Actualizar perfilfinal module

--- src/perfil_final_inventario_de_felder/perfil_final_inventario_de_felder.module.ts
+++ src/perfil_final_inventario_de_felder/perfil_final_inventario_de_felder.module.ts
@@ -4,9 +4,12 @@ import { PerfilFinalInventarioDeFelder } from './perfil_final_inventario_de_feld
 import { PerfilFinalInventarioDeFelderController } from './perfil_final_inventario_de_felder.controller';
 import { PerfilFinalInventarioDeFelderService } from './perfil_final_inventario_de_felder.service';
 import { Grupos } from 'src/grupos/grupos.entity';
+import { UnidadesModule } from '../unidades/unidades.module';
+import { ObjetosAprendizaje } from 'src/unidades/entities/objetos_aprendizaje.entity';
+import { EstiloObjeto } from 'src/unidades/entities/estilo_objeto.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PerfilFinalInventarioDeFelder, Grupos])],
+  imports: [TypeOrmModule.forFeature([PerfilFinalInventarioDeFelder, Grupos, ObjetosAprendizaje, EstiloObjeto]), UnidadesModule],
   controllers: [PerfilFinalInventarioDeFelderController],
   providers: [PerfilFinalInventarioDeFelderService],
 })
