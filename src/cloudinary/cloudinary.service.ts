d15a649ecb9bebf2f7de7e155047bef73d2848c1
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-11-08 18:50:24 -0700

    agregar servicio para subir archivos

---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { UploadApiErrorResponse, UploadApiResponse, v2 as cloudinary } from 'cloudinary';
+import * as streamifier from 'streamifier';
+
+@Injectable()
+export class ClodunaryService {
+    /**
+     * Sube un archivo a Cloudinary
+     * @params file - Archivo de Express Multer 
+     * @params folder - Carpeta en Cloudinary donde se guardará el archivo
+     * @returns URL del archivo subido
+     */
+
+    async uploadFile(
+        file: Express.Multer.File,
+        folder: string = 'objetos-aprendizaje'
+    ): Promise<String> {
+        return new Promise((resolve, reject) => {
+            const uploadStream = cloudinary.uploader.upload_stream(
+                {
+                    folder: folder,
+                    resource_type: 'auto', //Detecta el tipo de archivo automáticamente
+                    //Configuracion adicional para diferentes tipos de archivos
+                    allowed_formats: ['jpg', 'png', 'pdf', 'mp4', 'mp3', 'docx', 'pptx', 'txt']
+                },
+                (error: UploadApiErrorResponse, result: UploadApiResponse) => {
+                    if (error) {
+                        return reject(error);
+                    } else {
+                        resolve(result.secure_url);
+                    }
+                }
+            );
+
+            //Convertir el buffer del archivo a stream y subirlo
+            streamifier.createReadStream(file.buffer).pipe(uploadStream);
+        });
+    }
+
+}
