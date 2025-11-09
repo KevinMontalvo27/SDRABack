0fccc29b8800a9bb2e77396ccd0f7e2331507add
KevinMontalvo27 <montalvokevin375@gmail.com>
2025-11-08 21:40:22 -0700

    agregar controlador para subir OA con archivo

--- src/unidades/controllers/objeto-aprendizaje.controller.ts
+++ src/unidades/controllers/objeto-aprendizaje.controller.ts
@@ -1,11 +1,17 @@
-import { Controller, Get, Post, Body, Param, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, BadRequestException, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { GenericController } from 'src/generic/generic.controller';
 import { ObjetosAprendizaje } from '../entities/objetos_aprendizaje.entity';
 import { ObjetosAprendizajeService } from '../services/objetos-aprendizaje.service';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateObjetoAprendizajeDto } from '../dtos/create-objeto-aprendizaje.dto';
 
 @Controller('objetos-aprendizaje')
 export class ObjetosAprendizajeController extends GenericController<ObjetosAprendizaje, ObjetosAprendizajeService> {
-    constructor(private readonly objetosAprendizajeService: ObjetosAprendizajeService) {
+    constructor(
+        private readonly objetosAprendizajeService: ObjetosAprendizajeService,
+        private readonly cloudinaryService: CloudinaryService   
+    ) {
         super(objetosAprendizajeService);
     }
 
@@ -14,9 +20,43 @@ export class ObjetosAprendizajeController extends GenericController<ObjetosApren
         return this.objetosAprendizajeService.find();
     }
 
+    //OBJETO DE APRENDIZAJE SIN ARCHIVO, DATOS SOLO EN EL BODY
     @Post()
     async create(@Body() entity: ObjetosAprendizaje) {
         return this.objetosAprendizajeService.create(entity);
+    }
+
+    /**
+     * Crear objeto de aprendizaje con archivo
+     *  Sube el archivo a Cloudinary y guarda la URL en la base de datos en el campo 'contenido'
+     */
+
+    @Post('upload')
+    @UseInterceptors(FileInterceptor('file'))
+    async createWithFile(
+        @UploadedFile() file: Express.Multer.File,
+        @Body() body: Partial<CreateObjetoAprendizajeDto>
+    ) {
+        if (!file) {
+            throw new BadRequestException('Archivo no proporcionado');
+        }
+
+        try{
+            // Subir el archivo a Cloudinary
+            const fileUrl = await this.cloudinaryService.uploadFile(file, 'objetos-aprendizaje');
+
+            //Crear el objeto de aprendizaje con la URL del archivo
+            const objetoAprendizaje = new ObjetosAprendizaje();
+            objetoAprendizaje.id_tema = body.id_tema;
+            objetoAprendizaje.id_type = body.id_type;
+            objetoAprendizaje.nombre = body.nombre;
+            objetoAprendizaje.descripcion = body.descripcion;
+            objetoAprendizaje.contenido = fileUrl.toString();
+            return this.objetosAprendizajeService.create(objetoAprendizaje);
+        } catch (error) {
+            throw new BadRequestException(`Error al subir el archivo: ${error.message}`);
+        }
+
     }
 
     @Get('tema/:idTema')
