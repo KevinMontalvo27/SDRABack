456b76f03588f6c2b29c169f6ccf87d0758eee22
KevinMontalvo27 <montalvokevin375@gmail.com>
2026-01-13 14:33:42 -0700

    change update column to updated

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',   
+
+    ],
+    credentials: true,                             // Permitir cookies
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0' );
