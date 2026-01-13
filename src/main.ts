af98e970b2f12e8ead1fdecba418b312e06a875a
KevinMontalvo27 <montalvokevin375@gmail.com>
2026-01-13 14:42:27 -0700

    main cors

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:4200',   
-
-    ],
-    credentials: true,                             // Permitir cookies
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-  });
+  app.enableCors();
 
   const port = process.env.PORT || 3000;
   await app.listen(port, '0.0.0.0' );
-  console.log(`App running on port ${port}`);
 }
 bootstrap();
